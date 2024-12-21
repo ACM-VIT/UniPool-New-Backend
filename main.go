1fa9c0a095caa9f05939018b8527e2cbfddcda0e
Yash Raj Singh <yashudps.ys@gmail.com>
2024-12-21 19:02:25 +0530

    feat: FetchUserRides

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func SetupRoutes(app *fiber.App) {
 	app.Post("/user", middleware.Authenticate, routes.CreateOrUpdateUser) // Create or update a user
 	app.Get("/user/all", routes.GetUsers)          // Gets all users
 	app.Get("/user/fetch/:id", routes.GetUserByID) // Gets details of a user by it's ID
+	app.Get(("/user/:id/rides"), routes.FetchUserRides)
 }
 func main() {
 	initializer.InitFirebase()
