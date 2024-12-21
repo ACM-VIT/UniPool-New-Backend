328bbd5fc13641dedcf4e2f95f8cd96034d44961
Yash Raj Singh <yashudps.ys@gmail.com>
2024-12-21 19:17:48 +0530

    feat: Booking CRUD routes

--- main.go
+++ main.go
@@ -25,7 +25,15 @@ func SetupRoutes(app *fiber.App) {
 	app.Post("/user", middleware.Authenticate, routes.CreateOrUpdateUser) // Create or update a user
 	app.Get("/user/all", routes.GetUsers)          // Gets all users
 	app.Get("/user/fetch/:id", routes.GetUserByID) // Gets details of a user by it's ID
-	app.Get(("/user/:id/rides"), routes.FetchUserRides)
+	app.Get(("/user/:id/rides"), routes.FetchUserRides) //Gets all the rides of a particular user
+
+	//Booking CRUD routes
+
+	app.Post("/booking/create", routes.CreateBooking)      // Creates a new booking
+	app.Get("/booking/list", routes.GetBookings)           // Retrieves all bookings
+	app.Get("/booking/:id", routes.GetBookingByID)         // Retrieves a specific booking by its ID
+	app.Patch("/booking/update/:id", routes.UpdateBooking) // Updates an existing booking
+	app.Delete("/booking/delete/:id", routes.DeleteBooking) // Deletes a booking
 }
 func main() {
 	initializer.InitFirebase()
