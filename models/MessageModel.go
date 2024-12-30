ff47512ec67c6e20cc8838b3602fe527bcc50ce7
Tanush Golwala <tanushgolwala@gmail.com>
2024-12-31 00:01:58 +0530

    fix : message model migration

--- models/MessageModel.go
+++ models/MessageModel.go
@@ -5,9 +5,9 @@ import "github.com/google/uuid"
 // Message model
 type Message struct {
 	BaseModel
-	RideID   uuid.UUID `gorm:"not null;uniqueIndex:idx_booking_ride_passenger" json:"ride_id" valid:"required~Ride ID is required"`
+	RideID   uuid.UUID `gorm:"not null;uniqueIndex:idx_message_ride_sender" json:"ride_id" valid:"required~Ride ID is required"`
 	Ride     Ride      `gorm:"foreignKey:RideID;references:ID" json:"ride" valid:"-"`
-	SenderID uuid.UUID `gorm:"foreignKey:UserID;references:ID" json:"user" valid:"-"`
-	Sender   User      `gorm:"foreignKey:PassengerID;references:ID" json:"passenger" valid:"-"`
+	SenderID uuid.UUID `gorm:"not null;uniqueIndex:idx_message_ride_sender" json:"sender_id" valid:"required~Passenger ID is required"`
+	Sender   User      `gorm:"foreignKey:SenderID;references:ID" json:"passenger" valid:"-"`
 	Content  string    `json:"content"`
 }
