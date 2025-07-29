c7c9b192e4ac90d3465251f7b6a8b3d0903523cf
theg <52027622+theg1239@users.noreply.github.com>
2025-07-30 02:02:44 +0530

    fix: enhance user details response with total hosted rides

--- routes/users/GetUser.go
+++ routes/users/GetUser.go
@@ -1,17 +1,35 @@
+
 package users
 
 import (
 	"unipool-backend/models"
-
+	"unipool-backend/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 // Function to get details of user.
+
 func GetUser(c *fiber.Ctx) error {
 	// Extract user info from locals
 	if user, ok := c.Locals("user").(models.User); ok {
+		var totalHostedRides int64
+		database.Database.Db.Model(&models.Ride{}).Where("host_user_id = ?", user.ID).Count(&totalHostedRides)
+
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"user": user,
+			"user": fiber.Map{
+				"id": user.ID,
+				"name": user.Name,
+				"email": user.Email,
+				"profile_picture_url": user.ProfilePictureURL,
+				"contact_number": user.ContactNumber,
+				"gender": user.Gender,
+				"yob": user.YOB,
+				"default_address": user.DefaultAddress,
+				"created_at": user.CreatedAt,
+				"updated_at": user.UpdatedAt,
+				// ...other fields as needed...
+				"total_hosted_rides": totalHostedRides,
+			},
 		})
 	}
 
