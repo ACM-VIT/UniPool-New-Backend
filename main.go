b0b2aafbe9ef09617b270f875f60773f10e031f1
Adheesh Garg <adheeshgarg0611@gmail.com>
2025-08-01 02:50:17 +0530

    fix: on delete constraint added

--- main.go
+++ main.go
@@ -103,11 +103,6 @@ func main() {
 
 	database.ConnectToDB()
 	
-	// Run database migrations
-	if err := database.InitializeMigrations(); err != nil {
-		log.Fatalf("Failed to run migrations: %v", err)
-	}
-
 	app.Use(middleware.Authenticate)
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Scared of Women✌️!")
--- models/MessageModel.go
+++ models/MessageModel.go
@@ -7,6 +7,6 @@ type Message struct {
 	BaseModel
 	RideID   uuid.UUID `gorm:"not null" json:"ride_id" valid:"required~Ride ID is required"`
 	SenderID uuid.UUID `gorm:"not null" json:"sender_id" valid:"required~Sender ID is required"`
-	Sender   User      `gorm:"foreignKey:SenderID" json:"sender"`
+	Sender   User      `gorm:"foreignKey:SenderID; constraint:OnDelete:SET NULL" json:"sender"`
 	Content  string    `gorm:"type:text;not null" json:"content" valid:"required~Content is required"`
 }
