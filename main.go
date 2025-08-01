67d2c2f642282b166a87f6169614fc835480408f
theg <52027622+theg1239@users.noreply.github.com>
2025-08-01 20:03:12 +0530

    fix: reorder user routes for clarity and maintainability

--- main.go
+++ main.go
@@ -41,14 +41,14 @@ func SetupRoutes(app *fiber.App) {
 	// User CRUD routes
 	app.Post("/user", users.CreateOrUpdateUser)    // Create or update a user
 	app.Get("/user/details", users.GetUser)        // Gets user details
-	app.Get("/user/:id", users.GetUserByID)        // Gets user details by ID
 	app.Get("/user/all", users.GetAllUsers)        // Gets all users
-	app.Delete("/user/delete", users.DeleteUser)   // Deletes a user
 	app.Get("/user/rides", users.FetchUserRides)   // Gets all the rides of a particular user
 	app.Get("/user/passengers", users.GetPassengers) // Gets all passengers the user has travelled with
 	app.Get("/user/default-address", users.GetDefaultAddress) // Gets user's default start address
 	app.Post("/user/default-address", users.SetDefaultAddress) // Sets user's default start address
 	app.Patch("/user/default-address", users.SetDefaultAddress) // PATCH also sets user's default start address
+	app.Get("/user/:id", users.GetUserByID)        // Gets user details by ID (must be after specific routes)
+	app.Delete("/user/delete", users.DeleteUser)   // Deletes a user
 
 	// User token management routes
 	app.Post("/users/me/token", users.UpdateUserToken)   // Update user's FCM token
